292a67d37792c3bc060fc0f4dcd9d0ef6bb106d6
MUSADDIQUE-P <101107903+MUSADDIQUE-P@users.noreply.github.com>
2022-11-21 12:33:44 +0530

    Update id.js

--- id.js
+++ id.js
@@ -1,24 +1,46 @@
-function generator() {
-  // Get value of Student name from form input 
-  const nameElement = document.getElementById("name");
-  const nameValue = nameElement.value;
-  // Assign the value of student name to generated card
-  const cardNameElement = document.getElementById("cardName");
-  cardNameElement.innerHTML = nameValue;
+const quote = [
+  " 1. When you have a dream, you've got to grab it and never let go.",
+  " 2. So many books, so little time",
+  " 3. I'm not afraid of death, I just don't want to",
+  " 4. I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+  " 5. You only live once, but if you do it right, once is enough.",
+  " 6. Nothing is impossible. The word itself says Im possible and I'm doing it",
+  " 7. I am not a product of my circumstances. I— Audrey Hepburn"
+];
 
-  // Get value of college name from form input 
-  const collegeNameValue = document.getElementById("collegename").value;
-  // Assign the value of college name to generated card
-  document.getElementById("cardCollegeName").innerHTML = collegeNameValue;
+const colours = ["red", "blue", "ff6347", "green", "Tomato", "DodgerBlue", "Violet"];
 
-  // Get value of location name from form input 
-  const locationValue = document.getElementById("location").value;
-  // Assign the value of location name to generated card
-  document.getElementById("cardLocation").innerHTML = locationValue;
+const colourst = ["yellow", "greenyellow", "darkgreen", "cyan"];
 
-  // Display final generated card to user     
-  document.getElementById("collegeCard").style.display = "block";
+
+function newQuote() {
+  const randomNumber = Math.floor(Math.random() * quote.length);
+  document.getElementById("Quote_Show").innerHTML = quote[randomNumber];
+
+  var element = document.getElementById("Quote_Show");
+  element.style.backgroundColor = colours[randomNumber];
+
+  var element = document.getElementById("Quote_Show");
+  element.style.color = colourst[randomNumber];
 
 }
 
 
+let hour = new Date().getHours();
+var span = document.getElementById("SKY");
+if (hour > 20) {
+  span.textContent = "NIGHT";
+}
+else {
+  span.textContent = "DAY";
+}
+
+// var date = new Date();
+// var current_time = date.getHours();
+// var span = document.getElementById("SKY");
+// if (current_time = 1) {
+//   span.textContent = "NIGHT";
+// }
+// else {
+//   span.textContent = "DAY";
+// }
